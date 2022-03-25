38c5d15f19fa4a15649d1b0aeeb487bb68d5ecdf
RSharapova <rz.sharapova@gmail.com>
2022-03-25 19:27:20 +0500

    1. Поправлены отступы 2.Удален импорт

--- src/main/java/ru/gazprombank/MyFirstProgram.java
+++ src/main/java/ru/gazprombank/MyFirstProgram.java
@@ -1,18 +1,16 @@
 package ru.gazprombank;
 
-import java.awt.*;
-
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    Point p1 = new Point(1, 3);
-    Point p2 = new Point(8, 6);
-    Point p3 = new Point(10, 9);
-    System.out.println("Расстояние между точками p1 (" + p1.x + "," + p1.y + ") и p2 (" + p2.x + "," + p2.y + ") = " + distance(p1, p2) + " см.");
-    System.out.println("Расстояние между точками p1 (" + p2.x + "," + p2.y + ") и p2 (" + p3.x + "," + p3.y + ") = " + Point.distance(p2, p3) + " см.");
-  }
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(8, 6);
+        Point p3 = new Point(10, 9);
+        System.out.println("Расстояние между точками p1 (" + p1.x + "," + p1.y + ") и p2 (" + p2.x + "," + p2.y + ") = " + distance(p1, p2) + " см.");
+        System.out.println("Расстояние между точками p1 (" + p2.x + "," + p2.y + ") и p2 (" + p3.x + "," + p3.y + ") = " + Point.distance(p2, p3) + " см.");
+    }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-  }
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+    }
 }
